f421d399f243edd427bcb43da6fa4ffd0f82e152
Stevenson Marquez Rangel <smarquez@kuenta.co>
2020-05-16 17:49:07 -0500

    fix config url

--- src/public/settings.ts
+++ src/public/settings.ts
@@ -3,7 +3,7 @@ import { CloudinarySettings } from "../interfaces";
 
 export default (http: AxiosInstance) => ({
   async cloudinary() {
-    const res = await http.get<CloudinarySettings>("/api/v1/config/cloudinary");
+    const res = await http.get<CloudinarySettings>("/api/v1/public/config/cloudinary");
     return res.data;
   },
 });
