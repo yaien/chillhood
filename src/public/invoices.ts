c2fb1370304bc055edb6e5849e9223d8f06f6dfb
Stevenson Marquez <smarquez@kuenta.co>
2020-06-22 00:16:43 -0500

    check epayco response endpoint

--- src/public/invoices.ts
+++ src/public/invoices.ts
@@ -7,4 +7,10 @@ export default (http: AxiosInstance) => ({
     const res = await http.get<Invoice>(url);
     return res.data;
   },
+  async check(epaycoRef: string) {
+    const res = await http.get<Invoice>("/api/v1/public/epayco/response", {
+      params: { ref_epayco: epaycoRef },
+    });
+    return res.data;
+  },
 });
